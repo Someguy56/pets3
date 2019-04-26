10ec774485c56a791056921464eaac955a132424
Max Lee <maxwell.a.lee56@gmail.com>
2019-04-26 16:41:15 -0400

    Added routing for get and post

--- index.php
+++ index.php
@@ -58,20 +58,20 @@
         echo "<a href='order'>Order a Pet</a>";
     });
 
-    $f3->route('GET /order', function()
+    $f3->route('GET|POST /order', function()
     {
         $view = new Template();
         echo $view->render("views/form1.html");
     });
 
-    $f3->route('POST /order2', function()
+    $f3->route('POST|GET /order2', function()
     {
         $_SESSION['animal'] = $_POST['animal'];
         $view = new Template();
         echo $view->render("views/form2.html");
     });
 
-    $f3->route('POST /results', function()
+    $f3->route('POST|GET /results', function()
     {
         $_SESSION['color'] = $_POST['color'];
         $view = new Template();
