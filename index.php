fcb94cebd7a88710bbb215ca0c25e16b43d1e8d6
Max Lee <maxwell.a.lee56@gmail.com>
2019-04-29 16:35:26 -0400

    Finished up the pair program

--- index.php
+++ index.php
@@ -65,6 +65,7 @@
         if(isset($_SESSION))
         {
             session_destroy();
+            session_start();
         }
 
         if(isset($_POST['animal'])) {
@@ -75,6 +76,7 @@
             }
             else {
                 $f3->set("errors['animal']", "Please enter an animal.");
+                $f3->set("previous", $animal);
             }
         }
 
@@ -82,16 +84,27 @@
         echo $view->render("views/form1.html");
     });
 
-    $f3->route('POST|GET /order2', function()
+    $f3->route('POST|GET /order2', function($f3)
     {
-        $_SESSION['animal'] = $_POST['animal'];
+
+        if(isset($_POST['color'])) {
+            $color = $_POST['color'];
+            if(validString($color) && $color !== "--Select a color--") {
+                $_SESSION['color'] = $color;
+                $f3->reroute('/results');
+            }
+            else {
+                $f3->set("errors['color']", "Please select a color.");
+            }
+        }
+
         $view = new Template();
         echo $view->render("views/form2.html");
     });
 
     $f3->route('POST|GET /results', function()
     {
-        $_SESSION['color'] = $_POST['color'];
+
         $view = new Template();
         echo $view->render("views/results.html");
     });
